9ac9df57710dc1caec077db0a3ed6db2c2262df2
Jeff <51359768+Jiang-shiyu@users.noreply.github.com>
2021-05-04 19:49:35 +0800

    Create en_cn_pre_handle.py

---
+++ en_cn_pre_handle.py
@@ -0,0 +1,64 @@
+import time
+import numpy as np
+import jieba
+import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+def pre_handle(inputfilename, outputfilename):
+	with open(inputfilename, 'r') as f:
+	    f_list = [line for line in f]
+
+	f_list_s = [i.split('\t') for i in f_list]
+	df = pd.DataFrame(f_list_s, columns=['cn','en'])
+
+	# 中文语料库处理：分词
+	cn_list=[jieba.lcut(s) for s in df.cn]
+	df.cn=cn_list
+
+	# 英文语料库处理：分词、lowercase、lemmatizer
+	wnl = WordNetLemmatizer()
+	tokenized_sentences = [word_tokenize(sentence) for sentence in df.en]
+
+	for i in range(len(tokenized_sentences)):
+	    for j in range(len(tokenized_sentences[i])):
+	        tokenized_sentences[i][j] = wnl.lemmatize(tokenized_sentences[i][j].lower())
+
+	df.en=tokenized_sentences
+
+	df.to_csv(outputfilename)
+
+	# 计算词频
+	cn_word_list = [w for s in df.cn for w in s]
+	en_word_list = [w for s in df.en for w in s]
+
+	cn_words_freq = pd.Series(cn_word_list).value_counts()
+	en_words_freq = pd.Series(en_word_list).value_counts()
+
+	# 准备好四个待计算共现词频的变量
+	en_list = df.en.to_list() 
+	cn_list = df.cn.to_list()
+	return en_words_freq, cn_words_freq, en_list, cn_list
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
